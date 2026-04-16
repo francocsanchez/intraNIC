471c3242c836da5369d4552bb98ba60b6acb1021
Franco Sanchez <francocsanchez@gmail.com>
2026-04-16 15:00:23 -0300

    delete admin liess

--- front/src/components/MenuAdminNavbarLiess.tsx
+++ front/src/components/MenuAdminNavbarLiess.tsx
@@ -1,9 +1,8 @@
 import { useAuth } from "@/hooks/useAuthe";
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
-import { CircleUserRound, MonitorCog, PowerOff, Users, Wrench } from "lucide-react";
+import { CircleUserRound, PowerOff, Wrench } from "lucide-react";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import Loading from "./Loading";
-import useRoleGuard from "@/hooks/useRoleGuard";
 
 type NavBarProps = {
   negocio: string;
@@ -25,32 +24,8 @@ export default function MenuAdminNavbarLiess({ negocio }: NavBarProps) {
     navigate("/login", { replace: true });
   };
 
-  const { allowed: buttonsAdmin } = useRoleGuard(["admin"]);
   return (
     <div className="flex items-center gap-6 text-sm text-gray-600">
-      {/* Administración */}
-      {buttonsAdmin && (
-        <Menu as="div" className="relative">
-          <MenuButton className="inline-flex items-center gap-1 hover:text-gray-900 transition">
-            <MonitorCog size={16} strokeWidth={1.25} />
-            Administración
-          </MenuButton>
-
-          <MenuItems anchor="bottom end" className="mt-3 w-52 rounded-xl border border-gray-200 bg-white shadow-lg focus:outline-none">
-            <MenuItem>
-              {({ focus }) => (
-                <Link
-                  to={`/admin/usuarios`}
-                  className={` px-4 py-2 text-sm flex items-center gap-2 relative ${focus ? "bg-gray-50 text-gray-900" : "text-gray-700"}`}
-                >
-                  <Users size={16} strokeWidth={1.25} />
-                  Usuarios
-                </Link>
-              )}
-            </MenuItem>
-          </MenuItems>
-        </Menu>
-      )}
 
       {/* Perfil */}
       <Menu as="div" className="relative">
